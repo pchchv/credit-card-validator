b9985d42c312f317897d6c1548239255b2ac1d30
pchchv <ipchchv@gmail.com>
2022-11-12 17:55:49 +0300

    Implement the Luhn Algorigthm

--- validator.cpp
+++ validator.cpp
@@ -13,6 +13,39 @@ bool isNumberString(const string &s)
     return true;
 }
 
+bool validator(string ccNum)
+{
+    int len = ccNum.length();
+    int doubleEvenSum = 0;
+
+    // Double every second digit, starting from the right
+    // If the result is a two-digit number,
+    // add both digits to get one single-digit number
+    // Finally, sum all the answers to obtain 'doubleEvenSum'
+
+    for (int i = len - 2; i >= 0; i -= 2)
+    {
+        int digit = ((ccNum[i] - 48) * 2);
+        if (digit > 9)
+        {
+            digit = (digit / 10) + (digit % 10);
+        }
+        doubleEvenSum += digit;
+    }
+
+    // Add every odd digit from the right to the 'doubleEvenSum'
+
+    for (int i = len - 1; i >= 0; i -= 2)
+    {
+        doubleEvenSum += (ccNum[i] - 48);
+    }
+
+    // Check if 'doubleEvenSum' is a multiple of 10
+    // If yes, it is a valid credit card number
+
+    return doubleEvenSum % 10 == 0 ? true : false;
+}
+
 int main()
 {
     string ccNum;
@@ -30,9 +63,11 @@ int main()
         }
         else if (!isNumberString(ccNum))
         {
-            cout << "Bad input! ";
+            cout << "Bad input!" << endl;
             continue;
         }
+
+        cout << (validator(ccNum) ? "Valid" : "Invalid") << endl;
     }
 
     return 0;
